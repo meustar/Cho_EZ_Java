6613116d2f4d367c245b537e74e423787fde9d6b
meustar <meustar829@gmail.com>
2024-05-30 21:55:01 +0900

    work-2

--- src/MapExample.java
+++ src/MapExample.java
@@ -4,6 +4,6 @@ import java.util.Map;
 public class MapExample {
     public static void main(String[] args) {
         Map<String, String> dictionary = new HashMap<>();
-        dictionary.put()
+
     }
 }
